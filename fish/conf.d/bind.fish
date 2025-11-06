bacc2614de0b67763230d9da37416fd0bde77a87
acd407 <acd407@qq.com>
2025-11-06 23:45:00 +0800

    new key bind for dtach/abduco

--- fish/conf.d/bind.fish
+++ fish/conf.d/bind.fish
@@ -1,3 +1,26 @@
+function dtach_escape
+    set pid $fish_pid
+    set final false
+
+    while test $pid -gt 1
+        if test -r "/proc/$pid/comm" -a -r "/proc/$pid/stat"
+            read -l comm < "/proc/$pid/comm"
+            if test "$comm" = dtach
+                if test $final = true
+                    kill -HUP $pid
+                    return 0
+                end
+                set final true
+            end
+            read -l stat_content < "/proc/$pid/stat"
+            set pid (string split " " -- $stat_content)[4]
+        else
+            break
+        end
+    end
+    return 1
+end
+
 if status is-interactive
     bind ctrl-x ''
     bind ctrl-v ''
@@ -18,4 +41,6 @@ if status is-interactive
     bind alt-backspace backward-kill-word
     bind alt-left prevd-or-backward-word
     bind alt-right nextd-or-forward-word
+
+    bind ctrl-\\ dtach_escape
 end
