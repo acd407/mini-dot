335a9e256c36ff51b215964da15fe4a6a07cbe8d
acd407 <acd407@qq.com>
2026-03-12 16:18:36 +0800

    remove fish_add_path

--- fish/conf.d/00_envs.fish
+++ fish/conf.d/00_envs.fish
@@ -1,40 +1,3 @@
-set -l CPU_COUNT (nproc)
-set -gx MAKEFLAGS "-j$CPU_COUNT"
-set -gx OPENBLAS_NUM_THREADS $CPU_COUNT
-set -gx CMAKE_BUILD_PARALLEL_LEVEL 8
-
-set -g fish_greeting
-
-for i in /{usr/{local/,},}{,s}bin
-    if not contains $i $PATH; and test -d $i
-        fish_add_path -a $i
-    end
-end
-
-if status is-interactive
-    if test -d $HOME/.bin
-        fish_add_path -a $HOME/.bin
-        if test -d $HOME/.bin/custom
-            fish_add_path -a $HOME/.bin/custom
-        end
-        if test -d $HOME/.bin/appimage
-            fish_add_path -a $HOME/.bin/appimage
-        end
-        if test -n "$WAYLAND_DISPLAY"
-            fish_add_path -a $HOME/.bin/wm
-        end
-    end
-    if test -d $HOME/.local/bin
-        fish_add_path -a $HOME/.local/bin
-    end
-    if test -d /usr/lib/ccache
-        fish_add_path /usr/lib/ccache
-        if test -d /usr/lib/ccache/bin
-            fish_add_path /usr/lib/ccache/bin
-        end
-    end
-end
-
 if status is-login
     set -l oldenv (mktemp -p "$PREFIX/tmp" env.old.XXXXXX)
     set -l newenv (mktemp -p "$PREFIX/tmp" env.new.XXXXXX)
@@ -62,4 +25,41 @@ if status is-login
     end
 
     rm -f $oldenv $newenv
+end
+
+set -l CPU_COUNT (nproc)
+set -gx MAKEFLAGS "-j$CPU_COUNT"
+set -gx OPENBLAS_NUM_THREADS $CPU_COUNT
+set -gx CMAKE_BUILD_PARALLEL_LEVEL 8
+
+set -g fish_greeting
+
+for i in /{usr/{local/,},}{,s}bin
+    if not contains $i $PATH; and test -d $i
+        set -a PATH $i
+    end
+end
+
+if status is-interactive
+    if test -d $HOME/.bin
+        set -a PATH $HOME/.bin
+        if test -d $HOME/.bin/custom
+            set -a PATH $HOME/.bin/custom
+        end
+        if test -d $HOME/.bin/appimage
+            set -a PATH $HOME/.bin/appimage
+        end
+        if test -n "$WAYLAND_DISPLAY"
+            set -a PATH $HOME/.bin/wm
+        end
+    end
+    if test -d $HOME/.local/bin
+        set -a PATH $HOME/.local/bin
+    end
+    if test -d /usr/lib/ccache
+        set -p PATH /usr/lib/ccache
+        if test -d /usr/lib/ccache/bin
+            set -p PATH /usr/lib/ccache/bin
+        end
+    end
 end
--- fish/conf.d/05_modules.fish
+++ fish/conf.d/05_modules.fish
@@ -1,15 +1,15 @@
 if status is-interactive
     if command -v --quiet npm
         if test -d $XDG_DATA_HOME/npm/bin
-            fish_add_path -a $XDG_DATA_HOME/npm/bin
+            set -a PATH $XDG_DATA_HOME/npm/bin
         end
         if test -d $HOME/node_modules/.bin
-            fish_add_path -a $HOME/node_modules/.bin
+            set -a PATH $HOME/node_modules/.bin
         end
     end
     if command -v --quiet cargo
         if test -d $HOME/.cargo/bin
-            fish_add_path -a $HOME/.cargo/bin
+            set -a PATH $HOME/.cargo/bin
         end
         set -gx RUSTUP_DIST_SERVER 'https://rsproxy.cn'
         set -gx RUSTUP_UPDATE_ROOT 'https://rsproxy.cn/rustup'
@@ -17,7 +17,7 @@ if status is-interactive
     if command -v --quiet go
         set -gx GOPROXY 'https://goproxy.cn'
         set -gx GOPATH "$XDG_DATA_HOME/go"
-        fish_add_path -a $GOPATH/bin
+        set -a PATH $GOPATH/bin
     end
     if command -v --quiet vivid
         set -gx LS_COLORS (vivid generate one-dark)
